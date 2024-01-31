ce3a14a7488d74013e9d4c3d7c2a93833ce07881
sinijustus <147924458+sinijustus@users.noreply.github.com>
2024-01-31 10:41:40 +0530

    Create LargestNumber.py

---
+++ LargestNumber.py
@@ -0,0 +1,10 @@
+a=input("enter the first number:")
+b=input("enter the second number:")
+c=input("enter the second number:")
+if(a>b and a>c):
+    print("the largest number is",a)
+elif(b>a and b>c):
+    print("The largest number is:",b)
+else:
+    print("The largest number is:",c)
+    
